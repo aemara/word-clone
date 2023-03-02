eb1b31dd6d7542603a1728833e0c7e8aebe066d5
Amr Emara <a.emara47@gmail.com>
2023-03-02 21:22:28 +0200

    finish exercise 3

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { range } from "../../utils";
+
+function Guess({ word }) {
+  return (
+    <p className="guess">
+      {range(5).map((index) => {
+        return (
+          <span key={index} className="cell">
+            {word ? word[index] : undefined}
+          </span>
+        );
+      })}
+    </p>
+  );
+}
+
+export default Guess;
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Guess from "../Guess";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
 
 function GuessResults({ guessResults }) {
   return (
     <div class="guess-results">
-      {guessResults.map((word) => {
-        return <p className="guess">{word}</p>;
+      {range(NUM_OF_GUESSES_ALLOWED).map((index) => {
+        return <Guess key={index} word={guessResults[index]} />;
       })}
     </div>
   );
